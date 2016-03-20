74969489927c02c73366184973a48e365167d5a0
Darshan Thaker <dbthaker@gmail.com>
2016-03-19 22:17:43 -0700

    added types.hs

---
+++ haskell/Types/types.hs
@@ -0,0 +1,13 @@
+{- Saying :type THING gives you type of that thing (i.e. Bool, Char, etc.)
+ - Haskell considers all strings as list of characters ([Char])
+ - Haskell supports type synonyms (String is a synonym of Char)
+ - "Hello World" :: String is valid
+-}
+
+{- Functions in Haskell also have a type!
+ - Calling :t on a function gives the following format
+ - FUNCTION :: INPUT_TYPE -> OUTPUT_TYPE
+ - Ex. not :: Bool -> Bool
+ - chr and ord functions convert from character to ASCII and vice versa
+-}
+
